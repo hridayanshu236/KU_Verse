d5e958b64e92216071d5d989b9a2f17a05e70dee
Hridayanshu Raj Acharya <148516090+hridayanshu236@users.noreply.github.com>
2024-09-21 11:59:07 +0545

    initial project setup

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function App() {
+
+  return (
+    <>
+     <div className='text-center font-extrabold text-6xl'>
+      <h1>Welcome to KU-Verse</h1>
+     </div>
+    </>
+  )
+}
+
+export default App
