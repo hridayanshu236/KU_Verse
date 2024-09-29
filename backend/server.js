f41b0331c024e7e098a0fb8ff2b47e1b8280e105
Hridayanshu Raj Acharya <148516090+hridayanshu236@users.noreply.github.com>
2024-09-29 19:17:02 +0545

    mongodb connection

--- backend/server.js
+++ backend/server.js
@@ -10,17 +10,12 @@ app.use(cors());
 app.use(express.json());
 
 // Database connection
-// mongoose
-//   .connect(process.env.MONGODB_URI, {
-//     useNewUrlParser: true,
-//     useUnifiedTopology: true,
-//   })
-//   .then(() => console.log("MongoDB connected"))
-//   .catch((err) => console.error(err));
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(() => console.log("MongoDB connected"))
+  .catch((err) => console.error(err));
 
 // Define routes
-
-
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
