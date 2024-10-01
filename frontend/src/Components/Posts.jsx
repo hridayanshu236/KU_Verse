e7e4e74e7b3305d2ff85799233f3d7dd62b30a34
Bhishm <prakash.rhythm780@gmai.com>
2024-10-01 15:19:15 +0545

    Half Completed Post prototype

--- frontend/src/Components/Posts.jsx
+++ frontend/src/Components/Posts.jsx
@@ -1,13 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Posts = () => {
+  const [voteCount, setVoteCount] = useState(0);
+  function upVote() {
+    setVoteCount((prevCount) => prevCount + 1);
+  }
+
+  function downVote() {
+    setVoteCount((prevCount) => prevCount - 1);
+  }
   return (
     <>
-      <div className="flex flex-col h-[700px] w-[640px] rounded p-3">
-        <div className="bg-blue-200 flex-1 rounded-t-lg">
+      <div className=" bg-slate-50 flex flex-col h-auto w-[640px] rounded p-2">
+        <div className="bg-blue-200 flex-1 rounded-t-lg mb-1">
           <div className="items-center flex flex-col p-2 m-1 justify-items-center">
             <img
-              src="./Assets/perth.jpg"
+              src="..\src\Assets\parth.png"
               className="w-[70px] h-[70px] rounded-full"
               alt="Profile_picture"
             />
@@ -20,14 +28,69 @@ const Posts = () => {
             <span className="text-gray-400 font-extralight">2 hours ago</span>
           </div>
         </div>
-        <div className="bg-slate-400 flex-[2] flex items-center justify-center">
+        <div className="  flex-[2] flex items-center justify-center rounded-b-lg">
           <img
-            src="./Assets/pencils.jpg"
+            src="..\src\Assets\Post_Components\pencils.jpg"
             alt="Post_profile"
-            className="h-[500px]"
+            className="h-[500px] mb-2"
           />
         </div>
-        <div className="bg-green-200 flex flex-row justify-between px-2"></div>
+        <div className="flex  flex-row justify-between px-2">
+          <button
+            type=""
+            className=" hover:bg-slate-200 w-[50px]"
+            onClick={upVote}
+          >
+            {" "}
+            <div className="flex justify-center">
+              <img
+                src="..\src\Assets\Post_Components\circle-arrow-up.png"
+                alt="Comment_box"
+              />
+            </div>
+          </button>
+          <span className="text-red-500">{voteCount}</span>
+          <button
+            type=""
+            className=" hover:bg-slate-200 w-[50px]"
+            onClick={downVote}
+          >
+            {" "}
+            <div className="flex justify-center">
+              <img
+                src="..\src\Assets\Post_Components\circle-arrow-down.png"
+                alt="Comment_box"
+              />
+            </div>
+          </button>
+          <button type="" className=" hover:bg-slate-200 w-[50px]">
+            {" "}
+            <div className="flex justify-center">
+              <img
+                src="..\src\Assets\Post_Components\message-circle.png"
+                alt="Comment_box"
+              />
+            </div>
+          </button>
+          <button type="" className=" hover:bg-slate-200 w-[50px]">
+            {" "}
+            <div className="flex justify-center">
+              <img
+                src="..\src\Assets\Post_Components\share-2.png"
+                alt="Comment_box"
+              />
+            </div>
+          </button>
+          <button type="" className=" hover:bg-slate-200 w-[50px]">
+            {" "}
+            <div className="flex justify-center">
+              <img
+                src="..\src\Assets\Post_Components\bookmark-plus.png"
+                alt="Comment_box"
+              />
+            </div>
+          </button>
+        </div>
       </div>
     </>
   );
