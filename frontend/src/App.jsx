aff0d56c20d0350e66ef2446500e523956bfad51
Bhishm <prakash.rhythm780@gmai.com>
2024-10-01 11:12:52 +0545

    Added Pseudo route for "Post" Css model

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,16 @@
-import React from 'react'
+import React from "react";
 import { Route, Routes } from "react-router-dom";
 import Chats from "./pages/Chats";
+import Posts from "./Components/Posts";
 function App() {
-
   return (
     <>
       <Routes>
         <Route path="/chats" element={<Chats />} />
-
+        <Route path="/posts" element={<Posts />} />
       </Routes>
     </>
-  );               
+  );
 }
 
-export default App
+export default App;
