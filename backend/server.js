be5f324ed49ecf90d996986a165fd697de997491
SachinSrestha <sachinsrestha934@gmail.com>
2024-10-03 11:31:18 +0545

    Added functionality for register and login user

--- backend/server.js
+++ backend/server.js
@@ -8,12 +8,15 @@ const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
+app.use("/api/user", require("./routes/userRoutes"));
+app.use("/api/auth", require("./routes/authRoutes"));
+app.use("/api/post", require("./routes/postRoutes"));
 
 // Database connection
-mongoose
-  .connect(process.env.MONGODB_URI)
-  .then(() => console.log("MongoDB connected"))
-  .catch((err) => console.error(err));
+// mongoose
+//   .connect(process.env.MONGODB_URI)
+//   .then(() => console.log("MongoDB connected"))
+//   .catch((err) => console.error(err));
 
 // Define routes
 app.listen(PORT, () => {
