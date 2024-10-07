2e2ae64d054dc3873d84bfff9a160a841b9a5d15
SachinSrestha <sachinsrestha934@gmail.com>
2024-10-07 17:06:01 +0545

    Added delete and retreive post and updating post count of user

--- backend/controllers/postController.js
+++ backend/controllers/postController.js
@@ -2,11 +2,13 @@ const asyncHandler = require("express-async-handler");
 const Post = require("../models/postModel");
 const getDataURI = require("../utilities/generateURL");
 const cloudinary = require("../utilities/cloudinary");
+const User = require("../models/userModel");
 
 const createPost = asyncHandler(async (req,res) =>{
     const {caption} =req.body;
 
     const userID = req.user._id;
+    const currentUser = await User.findById(req.user._id);
 
     const file = req.file;
     const fileURI = getDataURI(file);
@@ -33,8 +35,43 @@ const createPost = asyncHandler(async (req,res) =>{
         },
     })
     await post.save();
+    
+    currentUser.posts.push(post._id);
+    await currentUser.save();
 
     res.status(201).json({message:"Post created succesfully",post})
 })
 
-module.exports = {createPost};
+const deletePost = asyncHandler(async (req,res) =>{
+    const post = await Post.findById(req.params.id);
+    const image = post._id;
+    const currentUser = await User.findById(req.user._id);
+
+    if(!post){
+        res.status(404);
+        throw new Error("No Post found");
+    }
+    if (post.user.toString() !== req.user._id.toString()) {
+        res.status(403);
+        throw new Error("UnAuthorized");
+    }
+    const deletePostId = currentUser.posts.indexOf(image);
+
+    await cloudinary.uploader.destroy(image);
+    currentUser.posts.splice(deletePostId,1);
+
+    await post.deleteOne();
+    await currentUser.save();
+
+    res.status(200).json({message:"Post Deleted succesfully"});
+})
+
+
+const getAllPosts = asyncHandler(async(req,res)=>{
+    const posts = await Post.find()
+                            .select("-user")
+                            .sort({createdAt:-1});
+    
+    res.json({posts});
+})
+module.exports = {createPost,deletePost,getAllPosts};
--- backend/routes/postRoutes.js
+++ backend/routes/postRoutes.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const router = express.Router();
-const{createPost} = require("../controllers/postController");
+const{createPost,deletePost,getAllPosts} = require("../controllers/postController");
 const {isAuth} =require("../middleware/isAuthenticated");
 const uploadFile = require("../middleware/multer");
 
 router.route("/createpost").post(isAuth,uploadFile, createPost);
+router.route("/deletepost/:id").delete(isAuth, deletePost);
+router.route("/posts").get(isAuth, getAllPosts);
 
 module.exports = router;
