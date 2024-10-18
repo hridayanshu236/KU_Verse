12ef19dd1290ab3c0841486357ed651802922d18
DontHash <rhythambhandari69@gmail.com>
2024-10-18 11:45:12 +0545

    Jhyau Lagyo aba ekxin ma Garamla

--- frontend/src/pages/Feed.jsx
+++ frontend/src/pages/Feed.jsx
@@ -17,14 +17,45 @@ const Feed = () => {
           {/* RightSide Chat and Story Section  */}
           <div className="flex-[0.8] flex flex-col bg-yellow-400 pl-4 justify-end">
             <h1>Right</h1>
+            {/* Story Section  */}
             <div className="flex-[1.3] flex flex-col bg-blue-400 h-full">
-              <div className="flex-1 my-1 border-2 border-x-1"></div>
-              <div className="flex-1 my-1 border-2 border-x-1"></div>
-              <div className="flex-1 my-1 border-2 border-x-1"></div>
-              <div className="flex-1 my-1 border-2 border-x-1"></div>
-              <div className="flex-1 my-1 border-2 border-x-1"></div>
+              <div className="flex-1 my-1 border-2 border-x-1 border-black"></div>
+              <div className="flex-1 my-1 border-2 border-x-1 border-black"></div>
+              <div className="flex-1 my-1 border-2 border-x-1 border-black"></div>
+              <div className="flex-1 my-1 border-2 border-x-1 border-black"></div>
+              <div className="flex-1 my-1 border-2 border-x-1 border-black"></div>
             </div>
-            <div className="flex-1 bg-cyan-400 w-[200px] self-end"></div>
+            {/* Chat Heads  */}
+            <div className="flex-1 flex flex-col justify-between items-center bg-cyan-400 w-[200px] self-end p-2">
+              <button>
+                <img
+                  class="w-[80px] h-[80px] rounded-full"
+                  src="../src/Assets/parth.png"
+                  alt="Rounded avatar"
+                />
+              </button>
+              <button>
+                <img
+                  class="w-[80px] h-[80px] rounded-full"
+                  src="../src/Assets/profile.png"
+                  alt="Rounded avatar"
+                />
+              </button>
+              <button>
+                <img
+                  class="w-[80px] h-[80px] rounded-full"
+                  src="../src/Assets/sachin.png"
+                  alt="Rounded avatar"
+                />
+              </button>
+              <button>
+                <img
+                  class="w-[80px] h-[80px] rounded-full"
+                  src="../src/Assets/kadel.png"
+                  alt="Rounded avatar"
+                />
+              </button>
+            </div>
           </div>
         </div>
       </div>
