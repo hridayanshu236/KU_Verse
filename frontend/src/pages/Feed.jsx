a6989e9935ecdf6ca6de03b9b645d3e89be915e8
DontHash <rhythambhandari69@gmail.com>
2024-10-18 15:42:12 +0545

    Minor Updation

--- frontend/src/pages/Feed.jsx
+++ frontend/src/pages/Feed.jsx
@@ -16,41 +16,47 @@ const Feed = () => {
           </div>
           {/* RightSide Chat and Story Section  */}
           <div className="flex-[0.8] flex flex-col bg-yellow-400 pl-4 justify-end">
-            <h1>Right</h1>
             {/* Story Section  */}
             <div className="flex-[1.3] flex flex-col bg-blue-400 h-full">
-              <div className="flex-1 my-1 border-2 border-x-1 border-black"></div>
+              <div className="flex-1 my-1 border-2 border-x-1 border-black "></div>
               <div className="flex-1 my-1 border-2 border-x-1 border-black"></div>
               <div className="flex-1 my-1 border-2 border-x-1 border-black"></div>
               <div className="flex-1 my-1 border-2 border-x-1 border-black"></div>
               <div className="flex-1 my-1 border-2 border-x-1 border-black"></div>
             </div>
             {/* Chat Heads  */}
-            <div className="flex-1 flex flex-col justify-between items-center bg-cyan-400 w-[200px] self-end p-2">
+            <div className="flex-1 flex flex-col justify-between items-center bg-cyan-400 w-[120px] self-end p-2">
               <button>
                 <img
-                  class="w-[80px] h-[80px] rounded-full"
+                  className=" min-w[30px] min-h-[30px] max-w-[60px] rounded-full transform transition-transform duration-200 ease-in-out hover:scale-110 hover:shadow-2xl"
                   src="../src/Assets/parth.png"
                   alt="Rounded avatar"
                 />
               </button>
               <button>
                 <img
-                  class="w-[80px] h-[80px] rounded-full"
+                  className=" min-w[30px] min-h-[30px] max-w-[60px] rounded-full transform transition-transform duration-200 ease-in-out hover:scale-110 hover:shadow-2xl"
                   src="../src/Assets/profile.png"
                   alt="Rounded avatar"
                 />
               </button>
               <button>
                 <img
-                  class="w-[80px] h-[80px] rounded-full"
+                  className=" min-w[30px] min-h-[30px] max-w-[60px] rounded-full transform transition-transform duration-200 ease-in-out hover:scale-110 hover:shadow-2xl"
+                  src="../src/Assets/bhisma.png"
+                  alt="Rounded avatar"
+                />
+              </button>
+              <button>
+                <img
+                  className=" min-w[30px] min-h-[30px] max-w-[60px] rounded-full transform transition-transform duration-200 ease-in-out hover:scale-110 hover:shadow-2xl"
                   src="../src/Assets/sachin.png"
                   alt="Rounded avatar"
                 />
               </button>
               <button>
                 <img
-                  class="w-[80px] h-[80px] rounded-full"
+                  className=" min-w[30px] min-h-[30px] max-w-[60px] rounded-full transform transition-transform duration-200 ease-in-out hover:scale-110 hover:shadow-2xl"
                   src="../src/Assets/kadel.png"
                   alt="Rounded avatar"
                 />
