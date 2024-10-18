2eca952c534fe92f121df943a048f33ad29e9811
DontHash <rhythambhandari69@gmail.com>
2024-10-18 11:23:44 +0545

    Feed Layout design

--- frontend/src/pages/Feed.jsx
+++ frontend/src/pages/Feed.jsx
@@ -7,7 +7,26 @@ const Feed = () => {
         <div className="">
           <Navbar />
         </div>
-        <h1 className="text-center font-bold">Feed</h1>
+        <div className="flex flex-row h-[100vh]">
+          {/* left Section */}
+          <div className="flex-1 bg-slate-500">Left</div>
+          {/* Middle Post section */}
+          <div className="flex-[2] bg-green-500">
+            <h1>Middle</h1>
+          </div>
+          {/* RightSide Chat and Story Section  */}
+          <div className="flex-[0.8] flex flex-col bg-yellow-400 pl-4 justify-end">
+            <h1>Right</h1>
+            <div className="flex-[1.3] flex flex-col bg-blue-400 h-full">
+              <div className="flex-1 my-1 border-2 border-x-1"></div>
+              <div className="flex-1 my-1 border-2 border-x-1"></div>
+              <div className="flex-1 my-1 border-2 border-x-1"></div>
+              <div className="flex-1 my-1 border-2 border-x-1"></div>
+              <div className="flex-1 my-1 border-2 border-x-1"></div>
+            </div>
+            <div className="flex-1 bg-cyan-400 w-[200px] self-end"></div>
+          </div>
+        </div>
       </div>
     </>
   );
