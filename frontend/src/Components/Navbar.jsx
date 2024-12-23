96d96a2fe5db6685fbf85389a8a96f8975498b16
Hridayanshu Raj Acharya <148516090+hridayanshu236@users.noreply.github.com>
2024-12-23 21:00:52 +0545

    logout fixed

--- frontend/src/Components/Navbar.jsx
+++ frontend/src/Components/Navbar.jsx
@@ -29,7 +29,7 @@ const Navbar = () => {
   const handleLogout = async () => {
     try {
       await axios.post(
-        "⁦http://localhost:5000/api/auth/logout⁩",
+        "http://localhost:5000/api/auth/logout",
         {},
         {
           withCredentials: true,
